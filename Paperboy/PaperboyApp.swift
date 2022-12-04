45339ae5e1aa8328842c16e5902b4b28ae59dcd3
Riccardo Persello <riccardo.persello@icloud.com>
2022-12-04 17:52:55 +0100

    Initial Commit

---
+++ Paperboy/PaperboyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperboyApp.swift
+//  Paperboy
+//
+//  Created by Riccardo Persello on 04/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PaperboyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
